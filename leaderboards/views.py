f36794c8e17c8310418d93ffeef5597daafa7039
Matt Wiens <mwiens91@gmail.com>
2018-08-27 09:56:08 -0700

    Use TemplateView for homepage view

--- leaderboards/views.py
+++ leaderboards/views.py
@@ -1,11 +1,9 @@
 """Views for leaderboards."""
 
 from django.shortcuts import render
-from django.views import View
+from django.views.generic import TemplateView
 
 
-class HomeView(View):
-    """View for the homepage."""
-    def get(self, request):
-        """Display the homepage."""
-        return render(request, 'leaderboards/base.html')
+class HomeView(TemplateView):
+    """A view for the homepage."""
+    template_name = "leaderboards/base.html"
