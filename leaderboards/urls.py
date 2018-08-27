f572da36b1b02bdbcc8556a351b0cce4aaf79703
Matt Wiens <mwiens91@gmail.com>
2018-08-26 22:08:28 -0700

    Make the homepage view a class-based view

--- leaderboards/urls.py
+++ leaderboards/urls.py
@@ -6,7 +6,7 @@ from leaderboards import views
 
 
 urlpatterns = [
-    path(r'', views.home_page_view, name='home'),
+    path(r'', views.HomeView.as_view(), name='home'),
     path(r'login/', auth_views.LoginView.as_view(template_name='leaderboards/login.html'), name='login'),
     path(r'logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- leaderboards/views.py
+++ leaderboards/views.py
@@ -1,8 +1,11 @@
 """Views for leaderboards."""
 
 from django.shortcuts import render
+from django.views import View
 
 
-def home_page_view(request):
-    """View for the main page."""
-    return render(request, 'leaderboards/base.html')
+class HomeView(View):
+    """View for the homepage."""
+    def get(self, request):
+        """Display the homepage."""
+        return render(request, 'leaderboards/base.html')
