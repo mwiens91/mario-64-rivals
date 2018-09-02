a922e423f28a55b1313262df8258e46f867c925d
Matt Wiens <mwiens91@gmail.com>
2018-09-01 20:12:44 -0700

    Add test for instantiating models

--- leaderboards/tests.py
+++ leaderboards/tests.py
@@ -1,5 +1,61 @@
 """Contains tests for the leaderboards app."""
 
+import datetime
 from django.test import TestCase
+from .models import (
+    User,
+    Category,
+    Course,
+    CategoryRecord,
+    SixStarCourseRecord,
+    SevenStarCourseRecord,
+)
 
-# Create your tests here.
+
+class ModelTests(TestCase):
+    """Test that models instantiate correctly.
+
+    Note that events are created automatically when records are created.
+    """
+    def test_model_instantiation(self):
+        """Make sure models instantiate without errors."""
+        # Create a user
+        user = User.objects.create(
+            username='AzureDiamond',
+            password='hunter2',)
+
+        # Create a course
+        course = Course.objects.create(
+            name="course name",
+            course_number=69,
+            preview_image="fake_media_location",)
+
+        # Create a category
+        category = Category.objects.create(
+            name="category name",
+            description="description",
+            preview_image="fake_media_location",)
+
+        # Create a category record
+        CategoryRecord.objects.create(
+            user=user,
+            category=category,
+            time=datetime.timedelta(seconds=420),
+            date=datetime.datetime.now(),
+            video_url="fakeurl.com",)
+
+        # Create a six star course record
+        SixStarCourseRecord.objects.create(
+            user=user,
+            course=course,
+            time=datetime.timedelta(seconds=420),
+            date=datetime.datetime.now(),
+            video_url="fakeurl.com",)
+
+        # Create a seven star course record
+        SevenStarCourseRecord.objects.create(
+            user=user,
+            course=course,
+            time=datetime.timedelta(seconds=420),
+            date=datetime.datetime.now(),
+            video_url="fakeurl.com",)
